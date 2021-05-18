c3a3d8214ab4572698e15715c675a91d97479e8d
Matthias Opitz <opitz@gmx.de>
2021-05-18 20:30:37 +0100

    applied layout to login and register pages

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -32,8 +32,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect('/');
-//        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
